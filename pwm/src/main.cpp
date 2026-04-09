a44ad37ada01aec41d6140066f128e9c379c0344
Kristoforus Gwayne Donovan Muliadi <98434053+micropeltes@users.noreply.github.com>
2026-04-09 14:11:47 +0700

    love me not

--- pwm/src/main.cpp
+++ pwm/src/main.cpp
@@ -1,4 +1,4 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h> 
 
 const char* ssid     = "ESP01_PWM";
 const char* password = "12345678";
@@ -6,9 +6,14 @@ const char* password = "12345678";
 WiFiServer server(80);
 const int pwmPin = 2;
 
+int pwmValue = 0;
+int pwmFreq = 1000; // default 1 kHz
+
 void setup() {
   pinMode(pwmPin, OUTPUT);
-  analogWrite(pwmPin, 0);
+
+  analogWriteFreq(pwmFreq);   // set frekuensi awal
+  analogWrite(pwmPin, pwmValue);
   
   WiFi.softAP(ssid, password);
   server.begin();
@@ -20,20 +25,44 @@ void loop() {
 
   String req = client.readStringUntil('\n');
 
-  // Tangani request: /set?val=NNN
+  // ===== SET PWM =====
   if (req.indexOf("/set?val=") != -1) {
     int pos = req.indexOf("val=") + 4;
     int pwm = req.substring(pos).toInt();
     pwm = constrain(pwm, 0, 1023);
-    analogWrite(pwmPin, pwm);
+
+    pwmValue = pwm;
+    analogWrite(pwmPin, pwmValue);
 
     client.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nPWM=");
-    client.print(pwm);
+    client.print(pwmValue);
     client.println();
     return;
   }
 
-  // Response untuk selain /set
+  // ===== SET FREKUENSI =====
+  if (req.indexOf("/freq?val=") != -1) {
+    int pos = req.indexOf("val=") + 4;
+    int freq = req.substring(pos).toInt();
+
+    // batas aman ESP8266
+    freq = constrain(freq, 100, 5000);
+
+    pwmFreq = freq;
+    analogWriteFreq(pwmFreq);
+
+    // apply ulang duty
+    analogWrite(pwmPin, pwmValue);
+
+    client.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nFREQ=");
+    client.print(pwmFreq);
+    client.println();
+    return;
+  }
+
+  // ===== DEFAULT RESPONSE =====
   client.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
-  client.println("Gunakan: /set?val=0-1023");
+  client.println("Gunakan:");
+  client.println("/set?val=0-1023");
+  client.println("/freq?val=100-5000");
 }
